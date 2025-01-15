7b47edfc05ccbdc33b9e652caa9fc05110176570
Valmy_mabika <valmy.mbama@ayokai.com>
2025-01-15 14:44:15 +0100

    add connection to back

---
+++ src/services/auth/auth.ts
@@ -0,0 +1,40 @@
+// import React from "react";
+import axios from "axios";
+import { config } from "../config";
+
+export class Auth {
+  static async createAccount(accountData: any) {
+    console.log(accountData);
+
+    try {
+      const response = await axios.post(
+        `${config.base_url}/auth/register`,
+        accountData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      return response.data;
+    } catch (error) {
+      console.error("Erreur lors de la création du compte :", error);
+    }
+  }
+
+  static async login(data: any) {
+    console.log(data);
+
+    try {
+      const response = await axios.post(`${config.base_url}/auth/login`, data, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log("response", response);
+      return response.data;
+    } catch (error) {
+      console.error("Erreur lors de la création du compte :", error);
+    }
+  }
+}
