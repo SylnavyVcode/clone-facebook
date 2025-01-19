50f41acf8a490e09a2ff3d14c488732ca2d274e2
Valmy Mabika <valmymabika@gmail.com>
2025-01-19 02:12:12 +0100

    session cookies

---
+++ src/services/auth/session.ts
@@ -0,0 +1,30 @@
+/**
+ * Created by Sylnavy Valmy MABIKA
+ * Session Class
+ */
+import axios from "axios";
+import { config } from "../config";
+
+export class Session {
+  static async getSession() {
+    const session = localStorage.getItem('cookie')
+    console.log(session);
+    
+  }
+
+  static async login(data: any) {
+    console.log(data);
+
+    try {
+      const response = await axios.post(`${config.base_url}/auth/login`, data, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log("response", response);
+      return response.data;
+    } catch (error) {
+      console.error("Erreur lors de la création du compte :", error);
+    }
+  }
+}
